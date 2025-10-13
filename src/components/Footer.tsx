e06958f1c6aa50bfc40eeabc34a94ca721cf1279
Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>
2025-10-13 18:43:00 +0530

    links
    Signed-off-by: Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -145,7 +145,7 @@ export default function Footer() {
             <ul className="space-y-3">
               <li>
                 <a
-                  href="#"
+                  href="https://docs.kubestellar.io/release-0.28.0/readme/"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
                   Overview
@@ -239,7 +239,7 @@ export default function Footer() {
             <ul className="space-y-3">
               <li>
                 <a
-                  href="#"
+                  href="http://docs.kubestellar.io/release-0.28.0/Community/_index/"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
                   Get Involved
@@ -247,7 +247,7 @@ export default function Footer() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/programs"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
                   Programs
