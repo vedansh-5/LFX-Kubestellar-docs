42d26ac818ac7580b8cfe32e130f675bc710e292
Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>
2025-10-13 17:58:29 +0530

    footer IA, link tag
    Signed-off-by: Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import Image from "next/image";
 import { GridLines, StarField } from "./index";
+import Link from "next/link";
 
 export default function Footer() {
   useEffect(() => {
@@ -86,7 +87,8 @@ export default function Footer() {
                   <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
                 </svg>
               </a>
-              <a
+              <Link
+                target="_blank"
                 href="https://github.com/kubestellar"
                 className="w-7 h-7 rounded-lg flex items-center justify-center transition-colors duration-300"
               >
@@ -98,8 +100,9 @@ export default function Footer() {
                 >
                   <path d="M12 0C5.374 0 0 5.373 0 12 0 17.302 3.438 21.8 8.207 23.387c.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.300 24 12c0-6.627-5.373-12-12-12z" />
                 </svg>
-              </a>
-              <a
+              </Link>
+              <Link
+                target="_blank"
                 href="https://www.linkedin.com/company/kubestellar/"
                 className="w-7 h-7 rounded-lg flex items-center justify-center transition-colors duration-300"
               >
@@ -111,8 +114,9 @@ export default function Footer() {
                 >
                   <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                 </svg>
-              </a>
-              <a
+              </Link>
+              <Link
+                target="_blank"
                 href="https://www.youtube.com/@kubestellar"
                 className="w-7 h-7 rounded-lg flex items-center justify-center transition-colors duration-300"
               >
@@ -124,20 +128,20 @@ export default function Footer() {
                 >
                   <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
                 </svg>
-              </a>
+              </Link>
             </div>
           </div>
 
-          {/* Product Links */}
+          {/* Docs Links */}
           <div className="lg:col-span-2">
-            <h3 className="text-lg font-semibold text-white mb-4">Product</h3>
+            <h3 className="text-lg font-semibold text-white mb-4">Docs</h3>
             <ul className="space-y-3">
               <li>
                 <a
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Features
+                  Overview
                 </a>
               </li>
               <li>
@@ -145,7 +149,7 @@ export default function Footer() {
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Use Cases
+                  Install & Configure
                 </a>
               </li>
               <li>
@@ -153,7 +157,7 @@ export default function Footer() {
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Pricing
+                  Uses & Integrate
                 </a>
               </li>
               <li>
@@ -161,22 +165,30 @@ export default function Footer() {
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Roadmap
+                  User Guide & Support
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
+                >
+                  UI Tools
                 </a>
               </li>
             </ul>
           </div>
 
-          {/* Resources Links */}
+          {/* Contribute Links */}
           <div className="lg:col-span-2">
-            <h3 className="text-lg font-semibold text-white mb-4">Resources</h3>
+            <h3 className="text-lg font-semibold text-white mb-4">Contribute</h3>
             <ul className="space-y-3">
               <li>
                 <a
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Documentation
+                  Overview
                 </a>
               </li>
               <li>
@@ -184,7 +196,7 @@ export default function Footer() {
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Tutorials
+                  Code of Conduct
                 </a>
               </li>
               <li>
@@ -192,7 +204,7 @@ export default function Footer() {
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Blog
+                  Guidelines
                 </a>
               </li>
               <li>
@@ -200,22 +212,30 @@ export default function Footer() {
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Community
+                  License
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
+                >
+                  Onboarding
                 </a>
               </li>
             </ul>
           </div>
 
-          {/* Company Links */}
+          {/* Community Links */}
           <div className="lg:col-span-2">
-            <h3 className="text-lg font-semibold text-white mb-4">Company</h3>
+            <h3 className="text-lg font-semibold text-white mb-4">Community</h3>
             <ul className="space-y-3">
               <li>
                 <a
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  About
+                  Get Involved
                 </a>
               </li>
               <li>
@@ -223,7 +243,7 @@ export default function Footer() {
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Team
+                  Programs
                 </a>
               </li>
               <li>
@@ -231,7 +251,7 @@ export default function Footer() {
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Careers
+                  Ladder
                 </a>
               </li>
               <li>
@@ -239,7 +259,7 @@ export default function Footer() {
                   href="#"
                   className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
                 >
-                  Contact
+                  Partners
                 </a>
               </li>
             </ul>
